b0c739a8acd2380f260096a51bbfe9db150594a5
NiaNash <nia312005@gmail.com>
2026-04-21 21:26:40 -0500

    Timer Version 2
    Timer counts down and removes 1 heart when it reaches zero. Auto resets it's self and starts timer again

--- Typography and Games Project 2_Nia and Bruno/Assets/Scripts/Timer.cs
+++ Typography and Games Project 2_Nia and Bruno/Assets/Scripts/Timer.cs
@@ -7,26 +7,58 @@ public class Timer : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI _text;
-
-    [SerializeField] private float _timeRemaining;
-
+    [SerializeField] private float _startTime = 10f; //round length
     [SerializeField] private int _decimals;
-    
+
+    private float _timeRemaining;
+    private bool hasTriggered = false;
+
     void Start()
     {
-        
+        _timeRemaining = _startTime;
     }
+
     void Update()
     {
-        if (_timeRemaining < 0) return;
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
 
-        _timeRemaining -= Time.deltaTime;
-        if (_timeRemaining < 0) _timeRemaining = 0;
+            if (_timeRemaining <= 0)
+            {
+                _timeRemaining = 0;
+                TimeUp();
+            }
+        }
+
         UpdateText();
     }
 
     private void UpdateText()
     {
         _text.text = _timeRemaining.ToString($"F{_decimals}");
+    }
+
+    private void TimeUp()
+    {
+        if (hasTriggered) return;
+
+        hasTriggered = true;
+
+        Debug.Log("Time's up!");
+
+        HeartsControl.health -= 1;
+
+        if (HeartsControl.health < 0)
+            HeartsControl.health = 0;
+
+        ResetTimer(); //used for rounds
+    }
+
+    //optional for rounds
+    public void ResetTimer()
+    {
+        hasTriggered = false;
+        _timeRemaining = _startTime; //make this the same value as default time
     }
 }
